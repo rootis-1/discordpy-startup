ad55b53b695acfa087a958654e4238fd1510748c
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-06-17 21:49:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='_')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -16,7 +16,7 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def urutan(ctx):
+async def ウルタンは(ctx):
     await ctx.send('くさい')
 
 
