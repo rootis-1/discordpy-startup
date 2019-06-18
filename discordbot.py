1ce870d9215637c33df16e196d58e61734213e5f
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-06-18 17:29:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ import os
 import traceback
 import random
 
-bot = commands.Bot(command_prefix='_')
+#bot = commands.Bot(command_prefix='_')
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -43,4 +43,5 @@ async def ウルタンは(ctx):
         await ctx.send('かわいい')
         '''
 
-bot.run(token)
+#bot.run(token)
+client.run(token)
