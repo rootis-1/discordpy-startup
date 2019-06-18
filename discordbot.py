142bf6314af9afd1cf99d36819216059fe3ba684
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-06-18 17:20:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,7 +38,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
+    if message.content.startswith('_hello'):
         await message.channel.send('Hello!')
 
 
