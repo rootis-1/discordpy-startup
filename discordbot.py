9a192e4575d7fb6c4cd68913b1c6f0178996ea4a
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-06-20 16:25:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -67,8 +67,10 @@ async def ウルタンは(ctx):
         
 @bot.command()
 async def frlist(ctx):
+    await ctx.send('----------------------------------------')
     for i in frdic:
-        await ctx.send('| '+i.rjust(12)+' | '+str(frdic[i]).ljust(12)+' |')
+        await ctx.send('|\t'+i+'\t'+str(frdic[i]).ljust(12)+'\t|')
+        await ctx.send('----------------------------------------\n')
         
 @bot.command()
 async def frc(ctx,cord:str):
