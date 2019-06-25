0f45a18a309d90200cc9d4d1cfa52051478f3467
rootis-1 <52198995+rootis-1@users.noreply.github.com>
2019-06-26 07:29:01 +0900

    まいにちまいにち過疎

--- discordbot.py
+++ discordbot.py
@@ -16,18 +16,15 @@ async def on_command_error(ctx, error):
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name='電波人間のRPG free'))
 
+    
 @bot.command()
 async def m(ctx):
     await ctx.send('まいにち過疎')
     
+    
 @bot.event
 async def わかる(message):
    if message.content.startswith('まいにち過疎'):
-
- 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
     
     
 @bot.command()
