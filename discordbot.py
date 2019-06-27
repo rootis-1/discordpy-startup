d84f78d8c5402fb7577b92eba0fd6be83bb61003
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-06-27 22:14:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,14 +29,11 @@ async def dc(ctx):
 @bot.command()
 async def いちごおばけ(ctx):
     await ctx.send('さいつお！')
-    
+       
 @bot.event
 async def on_message(message):
     if message.content.startswith('口が悪いね、残念だがここでお別れだ'):
         await message.channel.send('やめなさい')
-    
-@bot.event
-async def on_message(message):
     if message.author.bot==True:return;
     if message.content.startswith('まいにち過疎'):
         await message.channel.send('わかる')
