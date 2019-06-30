5f47ec8b6ece7ce1c3d6d09947c388be804f6d71
rootis-1 <52198995+rootis-1@users.noreply.github.com>
2019-07-01 08:49:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,7 +35,7 @@ async def on_message(message):
     if message.content.startswith('口が悪いね、残念だがここでお別れだ'):
         await message.channel.send('もちろんy')
     if message.content.startswith('おはよう'):
-        await message.channel.send('おはよう！れ！')
+        await message.channel.send('おは')
     if message.author.bot==True:return;
     if message.content.startswith('まいにち過疎'):
         await message.channel.send('わかる')
