a03fedafe9452c78b837e84b9576e8e11f51952d
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-07-01 19:54:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -85,7 +85,7 @@ async def decore(ctx,target:str,cont:str):
       elif target=="s" or target=="strike" or target =="deleted" or target=="取り消し線":string="~~"
       elif target.startswith('取り消'):string="~~"
       elif target=="u" or target=="アンダーライン" or target=="アンダーバー" or target=="下線" or target=="underline":string="__"
-      elif target=="コード" or target=="code":string=="```"
+      elif target=="コード" or target=="code":string="
       elif target=="隠す" or target=="マスク" or target=="mask":string="||"
       await ctx.send(string+cont+string)
 
