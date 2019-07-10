8790b051d748217f807fcb648241f5ed9b271d5b
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-07-10 18:11:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -208,7 +208,7 @@ async def emo5000(ctx,string:str):
       
       #.replace("R",":ReTweet: ").replace("F",":favorite: ").replace("s",":Splat_masaback: ")
       
-      await ctx.send(string)
+      await ctx.send(string[:2000])
 
       '''
       string = string.replace("あ","a").replace("い","i").replace("う","u").replace("え","e").replace("お","o")\
